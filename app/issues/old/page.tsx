238a866280f6eecd413ea255f906139018550f12
Deep Solanki <workitoutdeep@gmail.com>
2025-06-08 17:15:18 +0530

    queryString parameters

--- app/issues/old/page.tsx
+++ app/issues/old/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import { sort } from "fast-sort";
 
 type Users = {
   id: number;
@@ -8,19 +9,36 @@ type Users = {
   email: string;
 };
 
-const ServerDataFetching = async () => {
-  const body = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data: Users[] = await body.json();
+interface Props {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+const ServerDataFetching = async ({ searchParams }: Props) => {
+  const sortOrder =  searchParams?.sortOrder === "desc" ? "desc" : "asc";
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data: Users[] = await res.json();
+
+  const sorted =
+    sortOrder === "desc"
+      ? sort(data).desc((user) => user.name)
+      : sort(data).asc((user) => user.name);
+
   return (
     <div className="pt-5">
       <ul>
-        {data.map((user) => (
+        {sorted.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
-      <Button>
-        <Link href="/issues/old?sortOrder=users">Users</Link>
-      </Button>
+      <div className="flex gap-4 mt-4">
+        <Button>
+          <Link href="/issues/old?sortOrder=asc">Sort Asc</Link>
+        </Button>
+        <Button>
+          <Link href="/issues/old?sortOrder=desc">Sort Desc</Link>
+        </Button>
+      </div>
     </div>
   );
 };
